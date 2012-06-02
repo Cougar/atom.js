acd57ed5e71a8a5838c61bd6666cbf60b2025744
Björn Ramberg <bjorn@burtcorp.com>
2012-06-02 20:01:01 +0200

    Refactor atom and add example.

---
+++ examples/server-with-udp.js
@@ -0,0 +1,18 @@
+var atom = require('../lib/atom');
+
+var server = new atom.Atom({
+  protocolFile: './protocol.json',
+  monitor: {
+    port: 1201
+  },
+  udp: [
+    {
+      address: '192.168.1.250',
+      port: 1100
+    }
+  ]
+});
+
+server.on('error', function(error) {
+  console.log('An error occured in Atom: ' + error);
+});
